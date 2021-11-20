4db1cf1ff8f7a590ec15d5d1f003b3c73e5b1c08
AngryRectangle <yashmel2000@gmail.com>
2021-11-20 10:21:48 +0300

    Add some code

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-# This is a sample Python script.
+import sys
 
-print('Hello, world!')
+input = sys.argv[0]
+output = sys.argv[1]
+
+file = open(input)
+data = file.readlines()
+file.close()
+
+dictionary = dict()
+for line in data:
+    words = line.split()
+    for word in words:
+        lower = word.lower()
+        if word not in dictionary:
+            dictionary[lower] = 0
+        dictionary[lower] += 1
+
+sum = 0
+for value in dictionary.values():
+    sum += value
+
+arr = []
+for line in data:
+    words = line.split()
+    for word in words:
+        lower = word.lower()
+        if word in dictionary:
+            arr.append(word + " " + str(dictionary[lower]))
+            dictionary.pop(lower)
+
+file = open(output, "w")
+file.close()
