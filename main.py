1556836cf791dfa06e888793ce02f68ce32fadb5
AngryRectangle <yashmel2000@gmail.com>
2021-11-20 14:31:49 +0300

    Refactor

--- main.py
+++ main.py
@@ -2,49 +2,52 @@ from pathlib import Path
 import re
 import string
 
+punc = string.punctuation
+punc = punc.replace("-", "")
+punc = punc.replace("'", "")
+
+
+def validateWord(word):
+    lower = word.lower()
+    if lower[-1] == "-" or lower[-1] == "'":
+        lower = lower[:-1]
+
+    if lower == "":
+        return None
+    return lower
+
+
+def getWords(line):
+    for l in punc:
+        line = line.replace(l, " ")
+
+    return list(validateWord(n) for n in line.split())
+
 
 def collector(input, output):
-    punc = string.punctuation
-    punc = punc.replace("-", "")
-    punc = punc.replace("'", "")
     dictionary = dict()
     for content in open(input, encoding='utf-8'):
-        content = content.replace("\n", " ")
-        for l in punc:
-            content = content.replace(l, " ")
-
-        result = content.split()
+        result = getWords(content)
         for word in result:
-            lower = word.lower()
-            if lower[-1] == "-" or lower[-1] == "'":
-                lower = lower[:-1]
-
-            if lower == "":
+            if word is None:
                 continue
-            if lower not in dictionary:
-                dictionary[lower] = 0
-            dictionary[lower] += 1
+
+            if word not in dictionary:
+                dictionary[word] = 0
+            dictionary[word] += 1
 
     arr = []
     for content in open(input, encoding='utf-8'):
-        content = content.replace("\n", " ")
-        for l in punc:
-            content = content.replace(l, " ")
+        for word in getWords(content):
+            if word is None:
+                continue
 
-        result = content.split()
-        for word in result:
-            lower = word.lower()
-            if lower[-1] == "-" or lower[-1] == "'":
-                lower = lower[:-1]
-
-            if lower in dictionary:
-                arr.append(lower + " " + str(dictionary[lower]))
-                dictionary.pop(lower)
+            if word in dictionary:
+                arr.append(word + " " + str(dictionary[word]))
+                dictionary.pop(word)
 
     file = open(output, "w", encoding="utf-8")
     file.write("\n".join(arr))
     if len(arr) > 0:
         file.write("\n")
     file.close()
-
-# collector("input.txt", "output.txt")
