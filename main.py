bdeedf680db7ea5fe0bf9b7b6572db0249b61975
AngryRectangle <yashmel2000@gmail.com>
2021-11-20 12:34:41 +0300

    Update code

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-import sys
+from pathlib import Path
+import re
 
-input = sys.argv[0]
-output = sys.argv[1]
 
-file = open(input)
-data = file.readlines()
-file.close()
+def collector(input, output):
+    content = ""
+    for line in open(input, encoding='utf-8'):
+        content += line
 
-dictionary = dict()
-for line in data:
-    words = line.split()
-    for word in words:
-        lower = word.lower()
-        if word not in dictionary:
+    re.UNICODE = True
+    prog = re.compile("[a-zа-я]+")
+    content = content.replace("\n", " ")
+    #content = content.replace("", " ")
+    #content = content.replace("", " ")
+    result = content.split()
+    dictionary = dict()
+    for word in result:
+        match = re.findall(prog, word.lower())
+        if match is None:
+            continue
+        lower = ""
+        for word in match:
+            lower+=word
+
+        if lower.isspace() or lower == "`" or lower == "" or lower == "-" or lower == "_":
+            continue
+
+        if lower not in dictionary:
             dictionary[lower] = 0
         dictionary[lower] += 1
 
-sum = 0
-for value in dictionary.values():
-    sum += value
+    sum = 0
+    for value in dictionary.values():
+        sum += value
 
-arr = []
-for line in data:
-    words = line.split()
-    for word in words:
-        lower = word.lower()
-        if word in dictionary:
-            arr.append(word + " " + str(dictionary[lower]))
+    arr = []
+    for word in result:
+        match = re.findall(prog, word.lower())
+        if match is None:
+            continue
+        lower = ""
+        for word in match:
+            lower += word
+
+        if lower.isspace() or lower == "`" or lower == "":
+            continue
+
+        if lower in dictionary:
+            arr.append(lower + " " + str(dictionary[lower]))
             dictionary.pop(lower)
 
-file = open(output, "w")
-file.close()
+    file = open(output, "w", encoding="utf-8")
+    file.write("\n".join(arr))
+    file.close()
+
+# collector("input.txt", "output.txt")
