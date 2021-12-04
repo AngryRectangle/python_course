29e9f24825de3fd9b933c34eac9640036a2bc89e
AngryRectangle <yashmel2000@gmail.com>
2021-12-04 22:56:29 +0300

    Add 4 homework

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import sys
-import functions
+import requests
+import json
+import plotly.express as px
+import pandas as pd
+from collections import namedtuple
 
-input = sys.argv[1]
-output = sys.argv[2]
-functions.collector(input, output)
+token_file = sys.argv[1]
+ips_file = "ips.txt"
+
+token = open(token_file).readline()
+ips = open(ips_file).readlines()
+payload = {'token': token}
+Key = namedtuple('Key', ['country', 'region', 'city'])
+result = {}
+for ip in ips:
+    content = requests.get(f'https://ipinfo.io/{ip}', params=payload).content
+    decoded = json.loads(content)
+    key = Key(decoded['country'], decoded['region'], decoded['city'])
+    if key not in result:
+        result[key] = 1
+    else:
+        result[key] += 1
+
+show = list(
+    {'country': key.country, 'region': key.region, 'city': key.city, 'count': value} for key, value in result.items())
+fig = px.sunburst(pd.DataFrame(show), path=['country', 'region', 'city'], values='count')
+fig.show()
