337a6adbb1ea4593fd496b45f42c6d7a2d1ce1c6
AngryRectangle <yashmel2000@gmail.com>
2021-12-24 12:24:33 +0300

    Homework 5 fix

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 class mydefaultdict(dict):
-    def __init__(self, default_factory):
-        super().__init__()
+    def __init__(self, default_factory, seq=None, **kwargs):
+        super().__init__(**kwargs) if seq is None else super().__init__(seq, **kwargs)
         self.factory = default_factory
-
     def __getitem__(self, y):
-        if y not in self:
-            self.__setitem__(y, self.factory())
+        if y not in self: self.__setitem__(y, self.factory())
         return super().__getitem__(y)
-
-
-s = 'mississippi'
-d = mydefaultdict(int)
-for k in s:
-    d[k] += 1
-
-print(sorted(d.items()))
